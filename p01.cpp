9879b2561c5dd8a78304a824d61996b6787d1950
Aman Chouhan <ak6927887@gmail.com>
2025-10-27 21:07:03 +0530

    Update p01.cpp

--- p01.cpp
+++ p01.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <stack>   // header file for stack
+using namespace std;
+
+int main() {
+    stack<int> s;  // create a stack of integers
+
+    // push elements into stack
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Top element: " << s.top() << endl; // shows top element (30)
+
+    s.pop(); // remove top element
+
+    cout << "After pop, top element: " << s.top() << endl;
+
+    cout << "Size of stack: " << s.size() << endl;
+
+    if (s.empty())
+        cout << "Stack is empty" << endl;
+    else
+        cout << "Stack is not empty" << endl;
+
+    return 0;
+}
 
